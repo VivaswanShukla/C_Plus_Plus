21c38cadd8b2be55c7470530b740f9a2af9e2e81
Vivaswan Shukla <107896809+VivaswanShukla@users.noreply.github.com>
2023-05-27 13:41:28 +0530

    Add files via upload

---
+++ tut7.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 5;
+    int b = 6;
+
+    // Comparison Operators
+    cout<<"Is a > b: "<< (a > b) << endl;
+    cout<<"Is a >= b: "<< (a >= b) << endl;
+    cout<<"Is a < b: "<< (a < b) << endl;
+    cout<<"Is a <= b: "<< (a <= b) << endl;
+    cout<<"Is a == b: "<< (a == b) << endl;
+    cout<<"Is a != b: "<< (a != b) << endl;
+    return 0;
+}
